2f5e7c45e4007cc9045844576406adb17dcf1883
Darko Pantic <gusar97@gmail.com>
2023-03-19 20:57:03 +0100

    Added search functionality, and select dropdown menu for data/ styling

--- filters.js
+++ filters.js
@@ -1,20 +1,67 @@
 $(document).ready(function() {
-    // Učitavanje JSON podataka
-    $.getJSON("dataset_1.json", function(data) {
-      // Prikazivanje igara u katalogu igara
-      var gamesCatalog = $("#game-catalog");
-      $.each(data, function(gameName, gameInfo) {
-        var gameDiv = $("<div></div>").addClass("game");
-        $("<img/>", {
-          src: gameInfo.url,
-          alt: gameName,
-          title: gameName
-        }).appendTo(gameDiv);
-        $("<h2></h2>").text(gameName).appendTo(gameDiv);
-        $("<p></p>").text(gameInfo.genre).appendTo(gameDiv);
-        $("<p></p>").text(gameInfo.style).appendTo(gameDiv);
-        gameDiv.appendTo(gamesCatalog);
-      });
-    });
+    // Funkcija za prikazivanje JSON podataka
+    function showData(jsonFile) {
+      $.getJSON(jsonFile, function(data) {
+        var gamesCatalog = $("#game-catalog");
+        gamesCatalog.empty(); // da bude prazan prethodni prikaz
+        $.each(data, function(gameName, gameInfo) {
+          var gameDiv = $("<div></div>").addClass("game");
+          $("<img/>", {
+            src: gameInfo.url,
+            alt: gameName,
+            title: gameName
+          }).appendTo(gameDiv);
+          $("<h2></h2>").addClass("gameCard").text(gameName).appendTo(gameDiv);
+          $("<p></p>").addClass("zanrStil").text( gameInfo.genre).appendTo(gameDiv);
+          $("<p></p>").addClass("zanrStil").text( gameInfo.style).appendTo(gameDiv);
+          gameDiv.appendTo(gamesCatalog);
+      })}
+)}
+
+
+    // Prikaz prvog JSON fajla na loadu stranice
+    showData("dataset_1.json");
+
+    // Promena prikaza po izboru u select option
+    $("#dataset-select").change(function() {
+      var selectedFile = $(this).val();
+      showData(selectedFile);
+    })
+
+
+
+
+      // Pretraga igara po nazivu
+      $("#search-form").off(function() {
+        var searchText = $("#game-name-input").val().toLowerCase();
+        $("#game-catalog .game").each(function() {
+        var gameName = $(this).find("h2").text().toLowerCase();
+        if (!gameName.includes(searchText)) {
+        $(this).hide();
+        } else {
+        $(this).show();
+        }
+        });
+        });
+        
+        // Reset pretrage igara po nazivu
+        $("#game-name-input").on("keyup", function() {
+        var searchText = $(this).val().toLowerCase();
+        $("#game-catalog .game").each(function() {
+        var gameName = $(this).find("h2").text().toLowerCase();
+        if (!gameName.includes(searchText)) {
+        $(this).hide();
+        } else {
+        $(this).show();
+        }
+        });
+        });
+
+
+          // Reset input polja
+  $("#reset-button").on("click", function() {
+    $("#game-name-input").val("");
+    $("#game-catalog .game").show();
   });
   
+})
